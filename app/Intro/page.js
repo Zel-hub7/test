54561d56e31cb700da8d7985ff9338203f9e9141
Zola <Zelalemyohannes75@gmail.com>
2024-09-24 16:39:05 +0300

    commited

--- app/Intro/page.js
+++ app/Intro/page.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function IntroPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
@@ -59,13 +61,13 @@ export default function IntroPage() {
       {/* Video Section */}
       <div className="w-full max-w-4xl mb-8">
         <iframe
-            src="https://drive.google.com/file/d/1Ouu2NLB1CLcw52GlbBQKqet0YTCyN5aB/preview"
-            width="100%"
-            height="500px" 
-            allowfullscreen
-            className="w-full h-auto rounded-lg border-[3px] border-[#F6AD55] shadow-[0_0_15px_rgba(255,215,0,0.8)] hover:shadow-[0_0_30px_rgba(255,215,0,1)] transition-transform transform hover:scale-105 duration-300 ease-in-out"
-            style={{ aspectRatio: '16/9', border: 'none' }} 
-            loading="lazy"
+          src="https://drive.google.com/file/d/1Ouu2NLB1CLcw52GlbBQKqet0YTCyN5aB/preview"
+          width="100%"
+          height="500px"
+          allowfullscreen
+          className="w-full h-auto rounded-lg border-[3px] border-[#F6AD55] shadow-[0_0_15px_rgba(255,215,0,0.8)] hover:shadow-[0_0_30px_rgba(255,215,0,1)] transition-transform transform hover:scale-105 duration-300 ease-in-out"
+          style={{ aspectRatio: "16/9", border: "none" }}
+          loading="lazy"
         ></iframe>
       </div>
 
@@ -73,9 +75,11 @@ export default function IntroPage() {
       <p className="text-lg text-white text-center max-w-2xl mb-8">
         Once You have watched the video above click the button below
       </p>
-      <button className="w-[10%] bg-gradient-to-r from-blue-900 via-blue-400 via-gray-600 to-gray-900 text-white font-bold py-2 px-4 rounded-full hover:scale-105 transform transition-all">
-        Next
-      </button>
+      <Link href="/step2">
+        <button className="w-[150%] bg-gradient-to-r from-blue-900 via-blue-400 via-gray-600 to-gray-900 text-white font-bold py-2 px-4 rounded-full hover:scale-105 transform transition-all">
+          Next
+        </button>
+      </Link>
     </div>
   );
 }
---
+++ app/step2/page.js
@@ -0,0 +1,50 @@
+export default function Step2() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      {/* Logo Section */}
+      <div className="mb-4 mt-7 text-center">
+        <img
+          src="https://rippleeffectfree.com/wp-content/uploads/2024/04/logo-bg-remove-extra.png"
+          alt="Logo"
+          className="w-24 h-auto mx-auto mb-2"
+        />
+        <h2 className="text-white text-3xl font-semibold">Welcome to Step 2</h2>
+      </div>
+
+      {/* Video Section */}
+      <div className="w-full max-w-4xl mb-8">
+        <iframe
+          src="https://drive.google.com/file/d/1Ouu2NLB1CLcw52GlbBQKqet0YTCyN5aB/preview"
+          width="100%"
+          height="500px"
+          allowfullscreen
+          className="w-full h-auto rounded-lg border-[3px] border-[#F6AD55] shadow-[0_0_15px_rgba(255,215,0,0.8)] hover:shadow-[0_0_30px_rgba(255,215,0,1)] transition-transform transform hover:scale-105 duration-300 ease-in-out"
+          style={{ aspectRatio: "16/9", border: "none" }}
+          loading="lazy"
+        ></iframe>
+      </div>
+
+      {/* Description Section */}
+      <p className="text-lg text-white text-center max-w-2xl mb-8">
+        Once You have watched the video above click the button below to book
+        your call
+      </p>
+
+      {/* Navigation Link */}
+      <a href="/" className="text-blue-200 underline hover:text-blue-300">
+        Go back to Home Page
+      </a>
+      <div className="w-full flex max-w-4xl justify-between mt-4 px-4">
+        <button className="bg-gradient-to-r from-blue-500 to-gray-900 text-white font-bold py-2 px-4 rounded-full hover:scale-105 transform transition-all">
+          Back
+        </button>
+        <button className="bg-gradient-to-r from-blue-500 to-gray-900 text-white font-bold py-2 px-4 rounded-full hover:scale-105 transform transition-all">
+          Book your Call
+        </button>
+        <button className="bg-gradient-to-r from-blue-500 to-gray-900 text-white font-bold py-2 px-4 rounded-full hover:scale-105 transform transition-all">
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
